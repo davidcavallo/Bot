9e8d044c836fdb8bff95a2c4b5f3e35d73ac0534
Tabassum Ali <david.cavallo@hotmail.com>
2024-09-21 09:44:30 +0530

    Update main.go

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "80"
+		port = "8080"
 	}
 	fmt.Println("Server is listening on port", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
