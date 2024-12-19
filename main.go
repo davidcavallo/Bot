59d7091ace55597aef93b840d2226fc0adbab7d3
Tabassum Ali <david.cavallo@hotmail.com>
2024-12-19 13:39:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -109,7 +109,8 @@ func fetchWebsiteInfo(website string) string {
         log.Printf("Could not create request: %v", err)
         return "An error occurred while processing your request."
     }
-    req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0")
+    req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0")
+    req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")	
     req.Header.Set("Accept-Language", "en-US,en;q=0.9")
 
     client := &http.Client{Timeout: 10 * time.Second}
